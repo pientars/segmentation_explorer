30522cb3b6c283b986a0d900ba2729fabd0c9584
Robert P <robert.s.pienta@gmail.com>
2016-10-18 03:20:53 -0400

    update to frontend, jquery is weak

--- static/js/gui.js
+++ static/js/gui.js
@@ -1,6 +1,8 @@
 $(document).on('change', ':file', function() {
   var input = $(this),
       numFiles = input.get(0).files ? input.get(0).files.length : 1,
+      derp = input.val()
+      console.log(derp)
       label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
   input.trigger('fileselect', [numFiles, label]);
 });
@@ -27,6 +29,26 @@ $(document).ready( function() {
     removeOnSpill: true
   });
 
+  dragula([document.getElementById('collapse2'), document.getElementById('pipeline-inner-tray')], {
+    copy: function (el, source) {
+    return source === document.getElementById('collapse2')
+  },
+  accepts: function (el, target) {
+    return target !== document.getElementById('collapse2')
+  },
+    removeOnSpill: true
+  });
+
+  dragula([document.getElementById('collapse3'), document.getElementById('pipeline-inner-tray')], {
+    copy: function (el, source) {
+    return source === document.getElementById('collapse3')
+  },
+  accepts: function (el, target) {
+    return target !== document.getElementById('collapse3')
+  },
+    removeOnSpill: true
+  });
+
   var pipe_width = $('#pipeline-tray').width()
   var pipe_height = $('#pipeline-tray').height()
   d3.select('.arrow-svg').append('line')
@@ -36,5 +58,21 @@ $(document).ready( function() {
     .attr('y2', pipe_height-150)
     .attr('marker-end', 'url(#arrow)')
     .style('stroke-width', '25')
-    .style('stroke', '#aaaaaa');
+    .style('stroke', '#e0e0e0');
+
+  $('#run-button').on('click', run_samples )
 });
+
+function run_samples(d){
+  var trilden = [];
+  console.log($('#pipeline-inner-tray > div > h1.kernel-label').contents())
+  $('#pipeline-inner-tray > div > h1.kernel-label').contents().each(function(d){
+    trilden.push({
+      'name': $(this).text(),
+    });
+  });
+
+  trilden.forEach(function(d){
+    console.log(d)
+  })
+}
