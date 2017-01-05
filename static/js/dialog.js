f0ad587e0221e2b7ff386840240ee9d69ac017c3
Robert P <robert.s.pienta@gmail.com>
2017-01-05 01:23:00 -0500

    curse you style issues

--- static/js/dialog.js
+++ static/js/dialog.js
@@ -1,6 +1,6 @@
 var app = require('electron').remote;
 var dialog = app.dialog;
-
+var fs = require('fs');
 document.getElementById('batch-dir-btn').addEventListener('click',function(){
     dialog.showOpenDialog( {properties:['openDirectory']},
       function (fileNames) {
@@ -8,6 +8,7 @@ document.getElementById('batch-dir-btn').addEventListener('click',function(){
             console.log("No file selected");
         }else{
             document.getElementById("batch-dir-input").value = fileNames[0];
+            input_dir = fileNames[0];
         }
     });
 },false);
@@ -19,6 +20,13 @@ document.getElementById('sample-file-btn').addEventListener('click',function(){
             console.log("No file selected");
         }else{
             document.getElementById("sample-file-file").value = fileNames[0];
+            sample_image = fileNames[0];
+            copy_file(sample_image, function(e) {
+              fn = sample_image.split('\\').pop().split('/').pop();
+              d3.select('.sample-tray').transition().duration(500).style('opacity', 1);
+              d3.select('.sample-img').attr('src','./static/sample/'+fn);
+            })
+
         }
     });
 },false);
@@ -30,6 +38,33 @@ document.getElementById('output-dir-btn').addEventListener('click',function(){
             console.log("No file selected");
         }else{
             document.getElementById("output-dir-input").value = fileNames[0];
+            output_dir = fileNames[0];
         }
     });
 },false);
+
+function copy_file(source, cb) {
+  var cbCalled = false,
+      fn = sample_image.split('\\').pop().split('/').pop();
+      target = './static/sample/'+fn;
+
+  var rd = fs.createReadStream(source);
+  rd.on("error", function(err) {
+    done(err);
+  });
+  var wr = fs.createWriteStream(target);
+  wr.on("error", function(err) {
+    done(err);
+  });
+  wr.on("close", function(ex) {
+    done();
+  });
+  rd.pipe(wr);
+
+  function done(err) {
+    if (!cbCalled) {
+      cb(err);
+      cbCalled = true;
+    }
+  }
+}
--- static/js/gui.js
+++ static/js/gui.js
@@ -13,14 +13,6 @@ var params = {},
 function run_gui(){
   add_kernel_options();
 
-  // $(document).on('change', ':file', function() {
-  //   var input = $(this),
-  //       numFiles = input.get(0).files ? input.get(0).files.length : 1,
-  //       derp = input.val()
-  //       console.log(derp)
-  //       label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-  //   input.trigger('fileselect', [numFiles, label]);
-  // });
 
   // $(':file').on('fileselect', function(event, numFiles, label) {
   //     var input = $(this).parents('.input-group').find(':text'),
